5ed43a3cdcc82fda6007fa4cc026d93e92489d71
Kirill Bulatov <kirill@zed.dev>
2025-09-02 12:38:19 +0300

    Map backend interfaces down to LSP fetching
    Co-authored-by: Lukas Wirth <lukas@zed.dev>

--- crates/proto/src/proto.rs
+++ crates/proto/src/proto.rs
@@ -511,6 +511,7 @@ lsp_messages!(
     (GetDeclaration, GetDeclarationResponse, true),
     (GetTypeDefinition, GetTypeDefinitionResponse, true),
     (GetImplementation, GetImplementationResponse, true),
+    (InlayHints, InlayHintsResponse, false),
 );
 
 entity_messages!(
@@ -839,6 +840,7 @@ impl LspQuery {
             Some(lsp_query::Request::GetImplementation(_)) => ("GetImplementation", false),
             Some(lsp_query::Request::GetReferences(_)) => ("GetReferences", false),
             Some(lsp_query::Request::GetDocumentColor(_)) => ("GetDocumentColor", false),
+            Some(lsp_query::Request::InlayHints(_)) => ("InlayHints", false),
             None => ("<unknown>", true),
         }
     }
